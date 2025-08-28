45ec998228eb4d9f584d6c1043dc52a5dea76f73
felipejbs <felipejeronimobs@gmail.com>
2025-08-27 23:58:08 -0300

    feat: adicionando dataframe

--- extract/get_price.py
+++ extract/get_price.py
@@ -1,14 +1,22 @@
 import requests
 from datetime import datetime
+import pandas as pd
 
 # URL para obter o preço do Bitcoin
 url = "https://api.coinbase.com/v2/prices/spot"
 response = requests.get(url)
 
 if response.status_code == 200:
-    informacoes = {"preco": response.json()["data"]["amount"],
-                   "ativo": response.json()["data"]["base"],
-                   "moeda": response.json()["data"]["currency"],
-                   "horario": datetime.now()
+    preco = response.json()["data"]["amount"]
+    ativo = response.json()["data"]["base"]
+    moeda = response.json()["data"]["currency"]
+    horario = datetime.now()
+
+    informacoes = {"preco": preco,
+                   "ativo": ativo,
+                   "moeda": moeda,
+                   "horario": horario
                    }
-    print(informacoes)
+    dataframe = pd.DataFrame([informacoes])
+    print(dataframe)
+    #print(informacoes)
