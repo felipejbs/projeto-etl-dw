a9dbda816f729da6595eca094e24474a1cd52436
felipejbs <felipejeronimobs@gmail.com>
2025-09-10 00:11:06 -0300

    fix: corrigindo erro do preço

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 def get_latest_bitcoin_price():
     conn = sqlite3.connect('data/bitcoin_prices.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT preco, data FROM prices ORDER BY data DESC LIMIT 1")
+    cursor.execute("SELECT preco, horario FROM prices ORDER BY horario DESC LIMIT 1")
     result = cursor.fetchone()
     conn.close()
     return result
@@ -14,6 +14,6 @@ st.title("Consulta do Preço do Bitcoin")
 result = get_latest_bitcoin_price()
 if result:
     preco, data = result
-    st.metric("Preço Atual do Bitcoin", f"R$ {preco:,.2f}", label=f"Atualizado em: {data}")
+    st.metric("Preço Atual do Bitcoin", f"USD {preco}", help=f"Atualizado em: {data}")
 else:
     st.warning("Nenhum dado encontrado no banco de dados.")
