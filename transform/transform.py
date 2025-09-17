4f6703fb55d82c0f9b5a620d1524e8b235c3c4ab
felipejbs <felipejeronimobs@gmail.com>
2025-09-16 23:58:57 -0300

    fix

--- transform/transform.py
+++ transform/transform.py
@@ -1,3 +1,11 @@
+import pandas as pd
+
 def transform_df(df):
-    new_transformed_df = df
-    return new_transformed_df
+    if 'horario' in df.columns:
+        df['horario'] = pd.to_datetime(df['horario']).dt.strftime('%d/%m/%Y %H:%M:%S')
+    if 'preco' in df.columns:
+        df['preco'] = [
+            "USD {:,.2f}".format(float(valor)).replace(",", "X").replace(".", ",").replace("X", ".")
+            for valor in df['preco']
+        ]
+    return df
